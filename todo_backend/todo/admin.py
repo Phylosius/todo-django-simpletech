371e9869ac39e76e551411785712a902b83fb1f2
Phylosius <hei.phylosius@gmail.com>
2024-07-18 16:01:51 +0300

    feat: moved Todo registration to special class

--- todo_backend/todo/admin.py
+++ todo_backend/todo/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Todo, TodoList
 
 
-@admin.register(Todo, TodoList)
+@admin.register(TodoList)
+class GenericAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
     pass
