3cce5e737a1bdaf50a2b623cfb47386eb6004389
Phylosius <hei.phylosius@gmail.com>
2024-07-18 15:41:52 +0300

    feat: added field list on Todo models

--- todo_backend/todo/models.py
+++ todo_backend/todo/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+
 class Todo(models.Model):
     title = models.CharField(max_length=255)
     due_date = models.DateTimeField()
     completed = models.BooleanField(default=False)
     favorite = models.BooleanField(default=False)
+
+    list = models.ForeignKey('TodoList', null=False, on_delete=models.CASCADE)
 
 
 class TodoList(models.Model):
