398c57eb5cf503286f1177d31e8de2bf7b5ce45a
Phylosius <hei.phylosius@gmail.com>
2024-07-23 14:11:41 +0300

    feat: made api call require authentication

--- todo_backend/todo/views.py
+++ todo_backend/todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from todo.models import Todo, TodoList
 from todo.serializers import TodoSerializer, TodoListSerializer
@@ -9,10 +10,12 @@ class TodoViewSet(viewsets.ModelViewSet):
 
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class TodoListViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
+    permission_classes = (IsAuthenticated,)
 
