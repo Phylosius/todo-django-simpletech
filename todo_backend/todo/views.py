a0af5c34f267b3d12cd52774555620c54f95c169
Phylosius <hei.phylosius@gmail.com>
2024-07-23 13:54:44 +0300

    feat: inisializaed serializers for todo.models

--- todo_backend/todo/views.py
+++ todo_backend/todo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from todo.models import Todo, TodoList
+from todo.serializers import TodoSerializer, TodoListSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoListViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializer
+
